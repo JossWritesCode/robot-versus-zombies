b8ffe7ab12882bc6ccdb66607f18bd9c476109ca
JossWritesCode <joscelynrose@gmail.com>
2020-06-06 22:36:06 -0400

    set up sass files.

---
+++ front-end/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <img src="#" alt="RVZ" />
+      <a href="#">Login</a>
+    </div>
+  );
+}
+
+export default Header;
