e8eafd3b65c8234d8cf169e241d0c56f871ba8e9
JossWritesCode <joscelynrose@gmail.com>
2020-06-06 23:10:04 -0400

    set up the header

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Logo from "../images/green-rvz.png";
 
 function Header() {
   return (
     <div className="header">
-      <img src="#" alt="RVZ" />
-      <a href="#">Login</a>
+      <img width="109" src={Logo} alt="RVZ" />
+      <a className="login" href="#">
+        Login
+      </a>
     </div>
   );
 }
