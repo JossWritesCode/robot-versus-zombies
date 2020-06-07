9339a9f976342e5e8d6eb4a513beb946f50b2550
JossWritesCode <joscelynrose@gmail.com>
2020-06-07 15:44:17 -0400

    made header

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "./Header";
-
+import Main from "./Main";
 function App() {
   return (
     <div className="app">
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ front-end/src/components/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div className="main">
+      <h1>Robot vs. Zombies</h1>
+    </div>
+  );
+}
+
+export default Main;
